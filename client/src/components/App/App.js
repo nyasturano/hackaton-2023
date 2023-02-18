0721b5660aa0a861e672b45f8d28dd148d7fb574
Lera Panicheva <Lera.53rus@yandex.ru>
2023-02-18 19:15:03 +0300

    .
    x

--- client/src/components/App/App.js
+++ client/src/components/App/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import Auth from '../../pages/Auth/Auth';
 import CompanyPanel from '../../pages/CompanyPanel/CompanyPanel';
+<<<<<<< Updated upstream
 import { Routes, Route } from 'react-router';
 
 const App = () => {
@@ -10,6 +11,14 @@ const App = () => {
           <Route path="/" element={<Auth/>}/>
           <Route path="/admin" element={<CompanyPanel/>}/>
       </Routes>
+=======
+import Table from '../Table/Table';
+
+const App = () => {
+  return (
+    <div className='App'>
+      <Table/>
+>>>>>>> Stashed changes
     </div>
   );
 }
--- client/src/components/Table/Table.js
+++ client/src/components/Table/Table.js
@@ -2,7 +2,61 @@ import './table.css';
 
 const Table = () => {
     return (
-        <div></div>
+        <div className='table'>
+            <table>
+                <th colspan="3">История отправления</th>
+                
+                <tr>
+                    <td>zxcxz</td>
+
+                    <td>xzczx</td>
+
+                    <td>zxczxc</td>
+                </tr>
+                <tr>
+                    <td>xzczxc</td>
+
+                    <td>zxcxzc</td>
+
+                    <td>xzcxz</td>
+                </tr>
+                <tr>
+                    <td>xzczxc</td>
+
+                    <td>xzczxc</td>
+
+                    <td>xzczxc</td>
+                </tr>
+                <tr>
+                    <td>xzczxc</td>
+
+                    <td>xzczxc</td>
+
+                    <td>xzczxc</td>
+                </tr>
+                <tr>
+                    <td></td>
+
+                    <td></td>
+
+                    <td></td>
+                </tr>
+                <tr>
+                    <td></td>
+
+                    <td></td>
+
+                    <td></td>
+                </tr>
+                <tr>
+                    <td></td>
+
+                    <td></td>
+
+                    <td></td>
+                </tr>
+            </table>
+        </div>
     );
 };
 
